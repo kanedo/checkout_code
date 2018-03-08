2660edb6f0616d6723f2d9bcc46649b2dbb76659
Gabriel Bretschner <bretschner@i6.informatik.rwth-aachen.de>
2018-03-08 14:25:36 +0100

    move script to correct location. add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='checkout_code',
+      version='0.1',
+      description='Tool to checkout a specific commit to a unique location',
+      url='',
+      author='Gabriel Bretschner',
+      author_email='bretschner@i6.informatik.rwth-aachen.de',
+      license='MIT',
+      packages=['checkout_code'],
+      zip_safe=False)
+
