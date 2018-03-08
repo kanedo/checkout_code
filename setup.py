49b4fd5fd1c02707c3ae74a84732f1ccf260cff9
Gabriel Bretschner <bretschner@i6.informatik.rwth-aachen.de>
2018-03-08 14:44:44 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='checkout_code',
-      version='0.1',
+      version='0.2',
       description='Tool to checkout a specific commit to a unique location',
       url='',
       author='Gabriel Bretschner',
