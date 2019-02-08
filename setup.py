3d18cdba70c84458bdc09976b92d88ce77b1696d
Gabriel Bretschner <software@kanedo.net>
2019-02-08 11:03:22 +0100

    added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 
 setup(name='checkout_code',
       version='0.5',
       description='Tool to checkout a specific commit to a unique location',
-      url='',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url='https://github.com/kanedo/checkout_code',
       author='Gabriel Bretschner',
       author_email='bretschner@i6.informatik.rwth-aachen.de',
       license='MIT',
